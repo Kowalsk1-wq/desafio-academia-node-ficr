c198beebfe62df223d11a7f3348190ce7bb2011d
VanMatheus <limabrot879@gmail.com>
2020-01-31 13:29:30 -0300

    Deploy

--- src/server.js
+++ src/server.js
@@ -3,13 +3,13 @@ const chalk = require('chalk')
 const figlet = require('figlet')
 const { version } = require('../package.json')
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log(
     chalk.green(
       `${figlet.textSync('API - Challenge', {
         font: 'Doom',
         horizontalLayout: 'full',
-      })} v${version}\n       http://localhost:4000`
+      })} v${version}\n       http://localhost:4000/api/curriculo`
     )
   )
 })
